0ffba4286f25586a2b51e4b0ffa55b8f917e6633
oumark <oumarfilston@gmail.com>
2022-06-06 07:28:07 -0700

    change index

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,10 +1,14 @@
 export { default as Button } from './Button';
 export { default as ThemeSettings } from './ThemeSettings';
 export { default as Sidebar } from './Sidebar';
+// eslint-disable-next-line import/no-cycle
 export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
+// eslint-disable-next-line import/no-cycle
 export { default as Cart } from './Cart';
+// eslint-disable-next-line import/no-cycle
 export { default as Chat } from './Chat';
+// eslint-disable-next-line import/no-cycle
 export { default as Notification } from './Notification';
 export { default as UserProfile } from './UserProfile';
 export { default as SparkLine } from './Charts/SparkLine';
